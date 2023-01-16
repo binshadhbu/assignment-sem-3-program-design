1bf8f3c024d42e7b8b87ecb1c996a445f1a5abd7
binshadhbu <97302579+binshadhbu@users.noreply.github.com>
2023-01-16 13:11:07 +0530

    Update ASSG6__2.c

--- ASSG6_B210517CS_BINSHADH/ASSG6__2.c
+++ ASSG6_B210517CS_BINSHADH/ASSG6__2.c
@@ -1,35 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
 struct Nodebase{
     int data;
     struct Nodebase *next;
 };
 typedef struct Nodebase* Node;
-
 struct Stackbase{
     Node top;
 };
 typedef struct Stackbase* Stack;
-
 Node create_node(int k){
     Node new_node=(Node)malloc(sizeof(int));
     new_node->data=k;
     new_node->next=NULL;
     return new_node;
 }
-
 int stack_empty(Stack S){
     return S->top==NULL;
 }
-
 void push(Stack S,int k){
     Node new_node=create_node(k);
     new_node->next=S->top;
     S->top=new_node;
 }
-
 int pop(Stack S){
     if(stack_empty(S)){
         printf("Empty ");
@@ -43,9 +36,6 @@ int pop(Stack S){
         return value;
     }
 }
-
-
-
 int main()
 {
     int k,num;
